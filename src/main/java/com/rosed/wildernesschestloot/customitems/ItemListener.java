ceaa60a8ca1c8e37e25d370beb083d9eeb007e97
CenturyOrb <122176094+CenturyOrb@users.noreply.github.com>
2023-12-06 02:07:29 -0500

    Added Sovereign Sword execute
    When kill entity (player or mobs) the sword regenerates back to full dura

--- src/main/java/com/rosed/wildernesschestloot/customitems/ItemListener.java
+++ src/main/java/com/rosed/wildernesschestloot/customitems/ItemListener.java
@@ -1,11 +1,15 @@
 package com.rosed.wildernesschestloot.customitems;
 
 import com.rosed.wildernesschestloot.util.Util;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
 
@@ -24,6 +28,68 @@ public class ItemListener implements Listener {
             return;
 
         // Get the item on the source's hand
+        ItemStack handItem = entityEquipment.getItemInMainHand();
+
+        // Check if the item is a custom item
+        CustomItem customItem = Util.getCustomItem(handItem);
+        if (customItem == null)
+            return;
+
+        // Check if the item should be triggered by this event
+        if (!customItem.triggers().contains(event.getClass()))
+            return;
+
+        // Execute the custom item
+        customItem.execute(source, event.getEntity());
+    }
+
+    /**
+     * Event executes custom item when a player gets killed by
+     * a player using a custom item in main hand
+     * @param event PlayerDeathEvent
+     */
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    public void onPlayerDeathByEntity(PlayerDeathEvent event) {
+
+        // Check if the source is a Player
+        Player source;
+        if (event.getPlayer().getKiller() == null)
+            return;
+        else source = event.getPlayer().getKiller();
+
+        // Get the item on the source's hand
+        EntityEquipment entityEquipment = source.getEquipment();
+        ItemStack handItem = entityEquipment.getItemInMainHand();
+
+        // Check if the item is a custom item
+        CustomItem customItem = Util.getCustomItem(handItem);
+        if (customItem == null)
+            return;
+
+        // Check if the item should be triggered by this event
+        if (!customItem.triggers().contains(event.getClass()))
+            return;
+
+        // Execute the custom item
+        customItem.execute(source, event.getEntity());
+    }
+
+    /**
+     * Event executes custom item when an Entity gets killed by
+     * a player using a custom item in main hand
+     * @param event EntityDeathEvent
+     */
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    public void onEntityDeathByPlayer(EntityDeathEvent event) {
+
+        // Check if the source is a Player
+        Player source;
+        if (event.getEntity().getKiller() == null)
+            return;
+        else source = event.getEntity().getKiller();
+
+        // Get the item on the source's hand
+        EntityEquipment entityEquipment = source.getEquipment();
         ItemStack handItem = entityEquipment.getItemInMainHand();
 
         // Check if the item is a custom item
--- src/main/java/com/rosed/wildernesschestloot/customitems/impl/SovereignSword.java
+++ src/main/java/com/rosed/wildernesschestloot/customitems/impl/SovereignSword.java
@@ -1,18 +1,24 @@
 package com.rosed.wildernesschestloot.customitems.impl;
 
 import com.google.common.collect.Lists;
+import com.rosed.wildernesschestloot.InstanceManager;
 import com.rosed.wildernesschestloot.customitems.CustomItem;
 import com.rosed.wildernesschestloot.util.Util;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
-import org.bukkit.event.Event;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.List;
 
@@ -20,13 +26,23 @@ public class SovereignSword implements CustomItem {
 
     @Override
     public void execute(LivingEntity executor, Entity... targets) {
-        Bukkit.broadcastMessage("Sovereign sword");
+
+        ItemMeta meta = executor.getEquipment().getItemInMainHand().getItemMeta();
+
+        new BukkitRunnable() {
+            @Override
+            public void run () {
+                ((Damageable) meta).setDamage(0);
+                executor.getEquipment().getItemInMainHand().setItemMeta(meta);
+            }
+        }.runTaskLater(InstanceManager.INSTANCE.getPlugin(), 1);
+
     }
 
     @Override
     public List<Class<?>> triggers() {
         List<Class<?>> list = Lists.newArrayList();
-        list.add(EntityDamageByEntityEvent.class);
+        list.add(EntityDeathEvent.class);
         return list;
     }
 
@@ -34,16 +50,19 @@ public class SovereignSword implements CustomItem {
     public ItemStack itemStack() {
         ItemStack sword = new ItemStack(Material.GOLDEN_SWORD);
         sword.editMeta(meta -> {
-            meta.displayName(Component.text("Sovereign Sword", NamedTextColor.BLUE));
+            meta.displayName(Component.text("Sovereign Sword", NamedTextColor.BLUE).decoration(TextDecoration.ITALIC, false));
             meta.addEnchant(org.bukkit.enchantments.Enchantment.DAMAGE_ALL, 4, true);
+            meta.addEnchant(Enchantment.LOOT_BONUS_MOBS, 3, true);
             meta.addEnchant(org.bukkit.enchantments.Enchantment.SWEEPING_EDGE, 10, true);
             meta.lore(Lists.newArrayList(
-                    Component.text("Sharpness IV", NamedTextColor.GRAY),
-                    Component.text("Sweeping Edge IV", NamedTextColor.GRAY),
-                    Component.text("Blade of the old ruler", NamedTextColor.GRAY),
+                    Component.text("Sharpness IV", NamedTextColor.GRAY).decoration(TextDecoration.ITALIC, false),
+                            Component.text("Looting III", NamedTextColor.GRAY).decoration(TextDecoration.ITALIC, false),
+                    Component.text("Sweeping Edge X", NamedTextColor.GRAY).decoration(TextDecoration.ITALIC, false),
+                    Component.text("Blade of the old ruler", NamedTextColor.DARK_PURPLE),
                     Component.empty(),
-                    Component.text("- Renews on frag", NamedTextColor.GREEN)
-            ));
+                    Component.text("- Renews on frag", NamedTextColor.GREEN).decoration(TextDecoration.ITALIC, false)
+            )
+            );
             meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         });
         return Util.saveCustomItem(sword, this);
