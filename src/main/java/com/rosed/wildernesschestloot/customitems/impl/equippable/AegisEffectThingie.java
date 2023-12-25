66be62d029a59f03c25781b8f37c1adefa1a51ca
MrMaurice211 <t_ulu_m@live.com.mx>
2023-12-25 14:44:00 -0800

    Changed code on AegisEffectThingie

--- src/main/java/com/rosed/wildernesschestloot/customitems/impl/equippable/AegisEffectThingie.java
+++ src/main/java/com/rosed/wildernesschestloot/customitems/impl/equippable/AegisEffectThingie.java
@@ -1,54 +1,59 @@
 package com.rosed.wildernesschestloot.customitems.impl.equippable;
 
-import org.bukkit.Bukkit;
+import com.google.common.collect.Lists;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
-import org.bukkit.World;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class AegisEffectThingie extends BukkitRunnable {
 
     /**
-     * This just a task taht keeps tracks of arrows and entities that are
-     * wusing Aegis Dispersor works by a runnable running ever 3 ticks
-     * checking all arrows if they are near any one of the aegis dispursor users
+     * This just a task that keeps tracks of arrows and entities that are
+     * using Aegis Dispersor works by a runnable running ever 3 ticks
+     * checking all arrows if they are near any one of the aegis dispersor users
      */
-
-    private List<LivingEntity> aegisUsers;
-
-    public AegisEffectThingie()   {
-        aegisUsers = new ArrayList<>();
-    }
+    private final List<LivingEntity> aegisUsers = Lists.newArrayList();
 
     /**
      * Checks each arrow if the arrow is near a player
      * if arrow is near a player then destroy the arrow
-     *
      */
     @Override
     public void run() {
-        List<Arrow> arrows = new ArrayList<>();
-        for (World world : Bukkit.getWorlds()) {
-            for (Entity entity : world.getEntities()) {
-                if (entity instanceof Arrow) arrows.add((Arrow) entity);
+        for (LivingEntity user : aegisUsers) {
+            for (Entity entity : user.getNearbyEntities(5, 5, 5)) {
+                if (entity instanceof Arrow arrow) {
+                    arrow.getWorld().spawnParticle(Particle.EXPLOSION_NORMAL, arrow.getLocation(), 1, 1);
+                    arrow.getWorld().playSound(arrow.getLocation(), Sound.ENTITY_ARROW_HIT_PLAYER, 1, 1);
+                    arrow.remove();
+                }
             }
         }
 
-        for (Arrow arrow : arrows) {
-            for (LivingEntity entity : aegisUsers) {
-                if (arrow.getLocation().distance(entity.getLocation()) < 5)
-                    Bukkit.getWorld(arrow.getWorld().getUID()).spawnParticle(Particle.EXPLOSION_NORMAL, arrow.getLocation(), 1, 1);
-                    Bukkit.getWorld(arrow.getWorld().getUID()).playSound(arrow.getLocation(), Sound.ENTITY_ARROW_HIT_PLAYER, 1, 1);
-                    arrow.remove();
-            }
-        }
+//        List<Arrow> arrows = new ArrayList<>();
+//        for (World world : Bukkit.getWorlds()) {
+//            for (Entity entity : world.getEntities()) {
+//                if (entity instanceof Arrow) arrows.add((Arrow) entity);
+//            }
+//        }
+//
+//        for (Arrow arrow : arrows) {
+//            for (LivingEntity entity : aegisUsers) {
+//                if (arrow.getLocation().distance(entity.getLocation()) < 5)
+//                    Bukkit.getWorld(arrow.getWorld().getUID()).spawnParticle(Particle.EXPLOSION_NORMAL, arrow.getLocation(), 1, 1);
+//                    Bukkit.getWorld(arrow.getWorld().getUID()).playSound(arrow.getLocation(), Sound.ENTITY_ARROW_HIT_PLAYER, 1, 1);
+//                    arrow.remove();
+//            }
+//        }
     }
 
-    public List<LivingEntity> getAegisUsers() { return aegisUsers; }
+    public List<LivingEntity> getAegisUsers() {
+        return aegisUsers;
+    }
+
 }
